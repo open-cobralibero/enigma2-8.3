3ec363d44bb24f5341048676df4727130708d35c
open-cobralibero <121136157+open-cobralibero@users.noreply.github.com>
2022-12-21 10:59:00 +0100

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -31,8 +31,8 @@ class About(Screen):
 		AboutText = _("Hardware: ") + about.getHardwareTypeString() + "\n"
 		cpu = about.getCPUInfoString()
 		AboutText += _("CPU: ") + cpu + "\n"
-		AboutText += _("Image: ") + about.getImageTypeString() + "\n"
 		AboutText += _("Build date: open-cobralibero")  + "\n"
+		AboutText += _("Image: ") + about.getImageTypeString() + "\n"
 		AboutText += _("Forum: https://cobraliberosat.net") + "\n"
 		AboutText += _("Last update: ") + about.getUpdateDateString() + "\n"
 
