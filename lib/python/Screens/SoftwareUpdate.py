536258cd87834b6d04826b4ab4c75bab6f31b8c0
open-cobralibero <121136157+open-cobralibero@users.noreply.github.com>
2022-12-22 19:24:52 +0100

    Update SoftwareUpdate.py

--- lib/python/Screens/SoftwareUpdate.py
+++ lib/python/Screens/SoftwareUpdate.py
@@ -127,15 +127,15 @@ class UpdatePlugin(Screen, ProtectedScreen):
 					elif 'en_EN' in message:
 						message = message['en_EN']
 					else:
-						message = _("The current image might not be stable.\nFor more information see %s.") % ("https://forums.openpli.org")
+						message = _("The current image might not be stable.\nFor more information see %s.") % ("https://cobraliberosat.net")
 
 			except Exception, e:
 				print "[SoftwareUpdate] status error: ", str(e)
-				message = _("The current image might not be stable.\nFor more information see %s.") % ("https://forums.openpli.org")
+				message = _("The current image might not be stable.\nFor more information see %s.") % ("https://cobraliberosat.net")
 
 		# or display a generic warning if fetching failed
 		else:
-			message = _("The status of the current image could not be checked because %s can not be reached.") % ("https://openpli.org")
+			message = _("The status of the current image could not be checked because %s can not be reached.") % ("https://feed.cobraliberosat.net")
 
 		# show the user the message first
 		if message is not None:
